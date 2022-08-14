2c011eab39db1fe97bd69334a772078350d18abb
Luis <luisriveros26@gmail.com>
2022-08-14 13:36:18 -0600

    cart page

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
+import CartPage from './pages/CartLanding/CartPage';
 import Success from './pages/Success';
 import { StoreProvider } from "./utils/GlobalState";
 import OrderHistory from './pages/OrderHistory';
@@ -56,6 +57,10 @@ function App() {
               <Route
                 path="/signup"
                 element={<Signup />}
+              />
+              <Route
+                path="/cartpage"
+                element={<CartPage />}
               />
               <Route
                 path="/orderHistory"
